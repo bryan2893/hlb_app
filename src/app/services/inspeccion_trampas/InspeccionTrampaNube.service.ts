18858695bdc1d217eee2665a4fb293af8194e337
Bryan <hernandezbryan2893@gmail.com>
2020-07-15 21:11:59 -0600

    insp trampa e insp traspatios/fincas listos.

--- src/app/services/inspeccion_trampas/InspeccionTrampaNube.service.ts
+++ src/app/services/inspeccion_trampas/InspeccionTrampaNube.service.ts
@@ -2,56 +2,70 @@ import { Injectable } from '@angular/core';
 import { HTTP,HTTPResponse } from '@ionic-native/http/ngx';
 import {InspeccionTrampaNubeSubida} from '../../../DTO/server/InspeccionTrampaNubeSubida';
 import {SyncInfoService} from '../../services/syncInfo/sync-info.service';
+import {AlmacenamientoNativoService} from '../../services/almacenamiento-interno/almacenamiento-nativo.service';
+import {Settings} from '../../../DTO/settings.dto'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class InspeccionTrampaNubeService {
 
-  private urlToDownload = 'http://hlb.ticofrut.com/api/inspeccion_trampas/obtener_pagina/';
-  private urlToUpload = 'http://hlb.ticofrut.com/api/inspeccion_trampas/sincronizar/';
-  private urlToCountRecords = 'http://hlb.ticofrut.com/api/inspeccion_trampas/contarRegistros/';
+  private urlToDownload = '/api/inspeccion_trampas/obtener_pagina/';
+  private urlToUpload = '/api/inspeccion_trampas/sincronizar/';
+  private urlToCountRecords = '/api/inspeccion_trampas/contarRegistros/';
 
-  constructor(private http: HTTP, private syncInfoService:SyncInfoService) {
+  constructor(private http: HTTP, private syncInfoService:SyncInfoService,
+    private almacenamientoNativoService:AlmacenamientoNativoService) {
     this.http.setHeader('*', String("Accept"), String("application/json"));
     this.http.setDataSerializer('json');
   }
   
   getInspTrampPage(pageNumber:number,amountPerPage:number,oldDays:number,pais:string){
 
-    console.log("Entró a intentar descargar inspecciones de trampas amarillas");
-
     return new Promise((resolve,reject)=>{
-      let completedUrl = this.urlToDownload + pageNumber + '/' + amountPerPage+'/'+oldDays+'/'+pais;
-      console.log("Estructura del request = "+completedUrl);
-      this.http.get(completedUrl,{},{}).then((response:HTTPResponse)=>{
-        console.log("Datos descargados = "+response.data);
-        resolve(response);
-      }).catch((e)=>{
-        reject(e);
+
+      this.almacenamientoNativoService.obtenerParametrosDeConfiguracion().then((parametros:Settings)=>{
+
+        let completedUrl = parametros.link_de_sincronizacion + this.urlToDownload + pageNumber + '/' + amountPerPage+'/'+ oldDays+'/'+ pais;
+
+        this.http.get(completedUrl,{},{}).then((response:HTTPResponse)=>{
+
+          resolve(response);
+        }).catch((e)=>{
+          reject(e);
+        });
+
+      }).catch((error)=>{
+        reject(error);
       });
+
     });
   }
 
   syncListOfInspTramp(listaDeInspeccionesTrampas:InspeccionTrampaNubeSubida[]){
+
     return new Promise((resolve,reject)=>{
-      
-      this.syncInfoService.getSyncInfo().then((info)=>{
 
-        let paqueteDeSincronizacion = {
-          registros:listaDeInspeccionesTrampas,
-          informacionDeSincronizacion:[
-            info
-          ]
-        };
+      this.almacenamientoNativoService.obtenerParametrosDeConfiguracion().then((parametros:Settings)=>{
+        this.syncInfoService.getSyncInfo().then((info)=>{
+
+          let paqueteDeSincronizacion = {
+            registros:listaDeInspeccionesTrampas,
+            informacionDeSincronizacion:[
+              info
+            ]
+          };
+    
+          this.http.post(parametros.link_de_sincronizacion + this.urlToUpload, paqueteDeSincronizacion,{}).then((response:HTTPResponse) => {
+            resolve(response);
+          }).catch((error)=>{
+            reject(error);
+          });
   
-        this.http.post(this.urlToUpload, paqueteDeSincronizacion,{}).then((response:HTTPResponse) => {
-          resolve(response);
-        }).catch((error)=>{
-          reject(JSON.stringify(error));
+        }).catch((error) =>{
+          reject(error);
         });
-
-      }).catch((error) =>{
+      }).catch((error)=>{
         reject(error);
       });
       
@@ -60,40 +74,51 @@ export class InspeccionTrampaNubeService {
 
   countLastDaysRecords(pais:string,lastDays:number){
       return new Promise((resolve,reject)=>{
-      
-        this.http.get(this.urlToCountRecords+'/'+pais+'/'+lastDays,{},{}).then((response:HTTPResponse)=>{
-          let respuestaTransformada = JSON.parse(response.data);
-          resolve(respuestaTransformada[0].CANTIDAD);
-        }).catch((e)=>{
-          reject(e);
+
+        this.almacenamientoNativoService.obtenerParametrosDeConfiguracion().then((parametros:Settings)=>{
+
+
+          this.http.get(parametros.link_de_sincronizacion+this.urlToCountRecords+'/'+pais+'/'+lastDays,{},{}).then((response:HTTPResponse)=>{
+            let respuestaTransformada = JSON.parse(response.data);
+            resolve(respuestaTransformada[0].CANTIDAD);
+          }).catch((e)=>{
+            reject(e);
+          });
+
+        }).catch((error)=>{
+          reject(error);
         });
 
       });
   }
 
   getPagesQuantity(rowsPerPage:number,pais:string,lastDays:number){
+
     return new Promise((resolve,reject)=>{
-      this.countLastDaysRecords(pais,lastDays).then((response:number)=>{
-        let quantity = response;
 
-        if(quantity === 0){
-          resolve(0);
-        }
-
-        let divisionResiduo = quantity % rowsPerPage;
-        let divsionEntera = Math.trunc(quantity / rowsPerPage);
-
-        let num_paginas = divsionEntera;
-
-        if(divisionResiduo > 0){
-            num_paginas += 1;
-        }
-        
-        resolve(num_paginas);
-      }).catch((error:any)=>{
-        reject(error);
-      });
+        this.countLastDaysRecords(pais,lastDays).then((response:number)=>{
+          let quantity = response;
+  
+          if(quantity === 0){
+            resolve(0);
+          }
+  
+          let divisionResiduo = quantity % rowsPerPage;
+          let divsionEntera = Math.trunc(quantity / rowsPerPage);
+  
+          let num_paginas = divsionEntera;
+  
+          if(divisionResiduo > 0){
+              num_paginas += 1;
+          }
+          
+          resolve(num_paginas);
+        }).catch((error:any)=>{
+          reject(error);
+        });
       
     });
+
   }
+  
 }
